d0ec2a2b9aa3b2482bfc12fa4395c569e19fcc02
cmosley <cmosley@beholdmedia.com>
2022-04-13 11:38:03 -0500

    sign in comp. and sign in functionality

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -5,6 +5,12 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
+import { 
+  getFirestore, 
+  doc, 
+  getDoc, 
+  setDoc 
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyD3tT5NfeowMnIMFMLnjwLB7YZCaDMgOn4",
@@ -27,3 +33,26 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth, additionalData) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot.exists());
+
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+  
+    try {
+      await setDoc(userDocRef, { 
+        displayName, 
+        email, 
+        createdAt 
+      });
+    } catch(error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  return userDocRef;
+}
